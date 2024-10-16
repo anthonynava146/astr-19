8ac4e1ce27704979bde5f29f5446bf4d57d401dc
anthonynava146 <anfnavar@ucsc.edu>
2024-10-16 14:24:07 -0700

    Session 3 prompt for Coding Journal 1

---
+++ Session3Prompt.py
@@ -0,0 +1,14 @@
+#Function
+def f(x):
+	return x ** 3 + 8
+
+def main():
+	#Set x to 9
+	x = 9
+
+	#Check to see if f(x) > 27
+	if(f(x) > 27):
+		print("YAY!")
+
+if __name__ == '__main__':
+	main()
