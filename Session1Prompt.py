039806952a001007038c3ee50e06d5215c1f7262
anthonynava146 <anfnavar@ucsc.edu>
2024-10-16 13:43:06 -0700

    Session 1 prompt for Coding Journal 1

---
+++ Session1Prompt.py
@@ -0,0 +1,20 @@
+#Print name
+def PrintName():
+	print("My name is Anthony Navarro.")
+
+#Print favorite movie
+def PrintFavMovie():
+	print("My favorite movie would have to be the original Beetlejuice.")
+
+#Print favorite food
+def PrintFavFood():
+	print("My favorite food are burgers.")
+
+#Main
+def main():
+	PrintName()
+	PrintFavMovie()
+	PrintFavFood()
+
+if __name__ == '__main__':
+	main()
